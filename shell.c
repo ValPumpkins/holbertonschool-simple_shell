358768e20ac4667ca4e4183aab8e0fa4ea044d15
Lucieruin <Lucie.wattez@hotmail.fr>
2023-08-17 05:14:55 -0700

    creat main.h and file shell.c

---
+++ shell.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+* main - Entry point
+* @argc: argument count
+* @argv: argument vector
+* Return: Always 0 (Success)
+*/
+int main(int argc, char *argv[])
+{
+	char *prompt = "Shell ðŸŽˆ";
+	char *line = NULL;
+	int exe;
+	size_t len = 0;
+
+	while (1)
+	{
+		printf("%s", prompt);
+		fflush(stdout);
+
+		if (getline(&line, &len, stdin) == -1)
+		{
+			printf("Exiting...\n");
+			break;
+		}
+		if (*line == '\n')
+			continue;
+
+		exe = execute(line);
+
+		if (exe == -1)
+			perror(argv[0]);
+	}
+	free(line);
+
+	return (0);
+}
